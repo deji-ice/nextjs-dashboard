97e719f2a0416857676ec71e64af2f7b16fb826d
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-09-19 13:32:23 +0100

    Add customers and invoices pages to dashboard

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">Customers</h1>
+      <p className="mt-2">This is the customers page.</p>
+    </div>
+  );
+};
+
+export default page;
